9f3319f9923fa7fb260e6dea41bcc96ff32df41d
Divyaansh Jain <divyaansh03anu@gmail.com>
2020-05-31 01:18:49 +0530

    new file

--- cnn.py
+++ cnn.py
@@ -1,64 +1,77 @@
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
 from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 import os
 
-def model_train(epoch_no,num_of_crp):
+def model_train(epoch,n):
+    batch_size = 32
+    num_classes = 10
+    epochs = epoch
 
-   epochs=epoch_no
-   model = Sequential()
-   model.add(Convolution2D(filters=32,
-                        kernel_size=(3,3),
-                        activation='relu',
-                   input_shape=(64, 64, 3)
-                       ))
-   model.add(MaxPooling2D(pool_size=(2, 2)))
+    # input image dimensions
+    img_rows, img_cols = 28, 28
 
-   if num_of_crp > 1:
-      model.add(Convolution2D(filters=32,
-                        kernel_size=(3,3),
-                        activation='relu',
-                        ))
-      model.add(MaxPooling2D(pool_size=(2, 2)))
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-   model.add(Flatten())
-   model.add(Dense(units=128, activation='relu'))
-   model.add(Dense(units=1, activation='sigmoid'))
-   model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
 
-   from keras_preprocessing.image import ImageDataGenerator
-   train_datagen = ImageDataGenerator(
-           rescale=1./255,
-           shear_range=0.2,
-           zoom_range=0.2,
-           horizontal_flip=True)
-   test_datagen = ImageDataGenerator(rescale=1./255)
-   training_set = train_datagen.flow_from_directory(
-           '/dataset/cat_or_dog/training_set/',
-           target_size=(64, 64),
-           batch_size=32,
-           class_mode='binary')
-   test_set = test_datagen.flow_from_directory(
-           '/dataset/cat_or_dog/test_set/',
-           target_size=(64, 64),
-           batch_size=32,
-           class_mode='binary')
-   model.fit(training_set,
-           steps_per_epoch=50,
-           epochs=epochs,
-           validation_data=test_set,
-           validation_steps=800)
-   accuracy = model.evaluate(test_set,verbose=0)[1]
-   accuracy = accuracy*100
-   return accuracy
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
 
-no_of_epoch = 1
-no_of_layer = 1
-my_model = model_train(no_of_epoch,no_of_layer)
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
+    model = Sequential()
+    
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    if n>1:
+        model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    a=score[1]*100
+    model.save("MNIST.h5")
+    os.system("mv /MNIST.h5 /mycode")
+    return a
+
+no_epoch=1
+no_layer=1
+accuracy_train_model=model_train(no_epoch,no_layer)
 f = open("accuracy.txt","w+")
-f.write(str(my_model))
+f.write(str(accuracy_train_model))
 f.close()
-os.system("mv /accuracy.txt /dataset/")
+os.system("mv /accuracy.txt /dataset")
